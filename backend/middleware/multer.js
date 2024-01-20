f01755bc7692db511d489b861c45b8aa2eff8a8c
umar1110 <umar10022004@gmail.com>
2024-01-20 19:08:19 +0500

    comit 6

--- backend/middleware/multer.js
+++ backend/middleware/multer.js
@@ -7,7 +7,9 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const storage = multer.diskStorage({
-  destination: path.resolve(__dirname, '../'),
+  // destination: path.resolve(__dirname, '../'),
+  destination: null,
+
   filename: function(req, file, cb) {
     cb(null, file.originalname);
   }
